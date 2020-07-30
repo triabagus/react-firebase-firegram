e1764328d64af3ffdb1f171d9bf6ce1943a47b32
triabagus <triatop9@gmail.com>
2020-07-30 14:14:46 +0700

    Component: Upload form

---
+++ src/comps/UploadForm.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const UploadForm = () => { 
+    // State
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+
+    // Validation file image
+    const types = ['image/png', 'image/jpeg'];
+
+    // Function Change Image
+    const changeHandlerImage = (e) => { 
+        let selected = e.target.files[0];
+
+        // Conditions file image upload from computer
+        if (selected && types.includes(selected.type)) {
+            setFile(selected);
+            setError('');
+        } else { 
+            setFile(null);
+            setError('Please select an image file (png or jpeg)');
+        }
+    }
+
+    return (
+        <form>
+            <input type="file" onChange={changeHandlerImage} />
+            <div className="output">
+                {error && <div className="error">{error}</div>}
+                {file && <div className="file">{file.name}</div>}
+            </div>
+        </form>
+    );
+}
+
+export default UploadForm;
